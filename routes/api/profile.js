6fc7f69636afe7f1a9da16067763b759b4f6a9fd
TuanLamUet <tupikano1@gmail.com>
2019-11-23 10:18:23 +0700

    [15] create profile model

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,9 +1,107 @@
-const express = require('express');
-const router = express.Router();
+const mongoose = require('mongoose');
 
-//@route GET api/profile
-//@desc Test route
-//@access Public
-router.get('/', (req, res) => res.send('Profile route'));
+const ProfileSchema = new mongoose.Schema({
+    user: {
+        type: mongoose.Schemaa.types.ObjectId,
+        ref:'user'
+    },
+     company: {
+         type: String 
+     },
+     status: {
+         type: String,
+         required: true
+     },
+     skills: {
+         type: [String],
+         required: true
+     },
+    bio: {
+        type: String
+    },
+    githubusername: {
+        type: String
+    },
+    experience: [
+        {
+            title: {
+                type: String,
+                required: true
+            },
+            company: {
+                type: String,
+                required: true
+            },
+            localtion: {
+                type: String
+            },
+            from: {
+                type: Date,
+                required: true
+            },
+            to: {
+                type: Date
+            },
+            current: {
+                type: Boolean,
+                defaut: false
+            },
+            description: {
+                type: String
+            },
+        }
+    ],
+    education: [
+        {
+            school: {
+                type: String,
+                required: true
+            },
+            degree: {
+                type: String,
+                required: true
+            },
+            fieldofstudy: {
+                type: String,
+                required: true
+            },
+            from: {
+                type: Date,
+                required: true
+            },
+            to: {
+                type: Date
+            },
+            current: {
+                type: Boolean,
+                default: false
+            },
+            description: {
+                type: String
+            }
+         }
+    ],
+    social: {
+        youtube: {
+            type: String
+        },
+        twitter: {
+            type: String
+        },
+        facebook: {
+            type: String
+        },
+        linkedin: {
+            type: String
+        },
+        instagram: {
+            type: String
+        }
+    },
+    date: {
+        type: Date,
+        default: Date.now 
+    }
+});
 
-module.exports = router;
+module.exports = Profile = mongoose.model('profile', ProfileSchema);
