b341f07d00274664e5b7e85370a1d9c30d115874
TuanLamUet <tupikano1@gmail.com>
2020-01-12 21:01:55 +0700

    setup redux

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,8 +1,51 @@
-import React from 'react';
+import React,{ Fragment, useState } from 'react';
+import { Link } from 'react-router-dom'
+
 
 const Login = () => {
+  const [formData, setFormData] = useState({
+    email: '',
+    password: '',   
+  });
+
+  const { email, password } = formData;
+  const onChange = e => setFormData({...formData, [e.target.name]: e.target.value})
+  const onSubmit = e => { 
+    e.preventDefault(); 
+      console.log('Success')
+  }
   return (
-    <h1>Login</h1>
+    <Fragment>
+      <h1 className="large text-primary">Đăng nhập</h1>
+      <p className="lead"><i className="fas fa-user"></i> Đăng nhập tài khoản của bạn</p>
+      <form className="form" onSubmit={ e => onSubmit(e)}>
+        <div className="form-group">
+          <input
+            type="email"
+            placeholder="Email Address" 
+            name="email" 
+            value={email}
+            onChange={e => onChange(e)}
+            requireq
+          />
+        </div>
+        <div className="form-group">
+          <input
+            type="password"
+            placeholder="Password"
+            name="password"
+            minLength="6"
+            value={password}
+            onChange={e => onChange(e)}
+          />
+        </div>
+
+        <input type="submit" className="btn btn-primary" value="Đăng nhập" />
+      </form>
+      <p className="my-1">
+        Bạn chưa có tài khoản? <Link to="/register">Đăng ký</Link>
+      </p>
+    </Fragment>
   )
 }
 
--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,4 +1,5 @@
-import React,{Fragment, useState} from 'react';
+import React,{ Fragment, useState } from 'react';
+import { Link } from 'react-router-dom'
 
 const Register = () => {
   const [formData, setFormData] = useState({
@@ -13,17 +14,16 @@ const Register = () => {
   const onSubmit = e => { 
     e.preventDefault();
     if(password !== password2) {
-      console.log('!!!!!!!!!!!!')
+      console.log('Passwords do not match');
     }
     else{
-      console.log(formData)
+      console.log('Success')
     }
-
   }
   return (
     <Fragment>
-      <h1 className="large text-primary">Sign Up</h1>
-      <p className="lead"><i className="fas fa-user"></i> Create Your Account</p>
+      <h1 className="large text-primary">Đăng ký</h1>
+      <p className="lead"><i className="fas fa-user"></i> Tạo tài khoản của bạn</p>
       <form className="form" onSubmit={ e => onSubmit(e)}>
         <div className="form-group">
           <input type="text"
@@ -43,8 +43,7 @@ const Register = () => {
             requireq
           />
           <small className="form-text"
-            >This site uses Gravatar so if you want a profile image, use a
-            Gravatar email</small
+            >Để có ảnh đại diện, vui lòng sử dụng email có ảnh của bạn</small
           >
         </div>
         <div className="form-group">
@@ -67,10 +66,10 @@ const Register = () => {
             onChange={e => onChange(e)}
           />
         </div>
-        <input type="submit" className="btn btn-primary" value="Register" />
+        <input type="submit" className="btn btn-primary" value="Đăng ký" />
       </form>
       <p className="my-1">
-        Already have an account? <a href="login.html">Sign In</a>
+        Bạn đã có tài khoản? <Link to="/login"> Đăng nhập</Link>
       </p>
     </Fragment>
   )
